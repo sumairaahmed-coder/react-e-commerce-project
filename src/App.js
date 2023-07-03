7d4678f67c84f9151dc0d7cc24f17845924d61ca
sumairaahmed <sumairaahmedcp@gmail.com>
2023-07-03 10:07:45 +0530

    added navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./categories.styles.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am Shop page</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
