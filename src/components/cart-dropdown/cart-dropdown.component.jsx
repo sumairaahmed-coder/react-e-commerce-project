48e5baaebcf395d33594b06ea279595c7896086d
sumairaahmed <sumairaahmedcp@gmail.com>
2023-07-12 11:36:02 +0530

    created cart-iten component and added the the add to cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,18 @@
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
+import CartItems from "../cart-items/cart-items.component";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => {
+          return <CartItems key={item.id} cartItem={item} />;
+        })}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
---
+++ src/components/cart-items/cart-items.component.jsx
@@ -0,0 +1,18 @@
+import "./cart-items.styles.scss";
+
+const CartItems = ({ cartItem }) => {
+  const { name, price, imageUrl, quantity } = cartItem;
+  return (
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
